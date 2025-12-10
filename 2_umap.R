04a4f4bf11ae37ab7d5a7e286dae1c068fd46c90
obruck <oscar.bruck@helsinki.fi>
2025-12-11 01:06:57 +0200

    Update 2_umap.R

--- 2_umap.R
+++ 2_umap.R
@@ -56,7 +56,7 @@ set.seed(seed1)
 # Select features for UMAP
 umap_data = df %>%
   dplyr::select(which(names(df)=="rbc_sample_proportion"):which(names(df)=="macrophages_nuclei_compactness_percentile_75"))
-umap_metadata = df
+umap_metadata = df #%>%
 ## Remove rows where all image values are NA
 remove_rows = rowSums(is.na(umap_data)) != ncol(umap_data)
 umap_data = umap_data[remove_rows,]
@@ -66,23 +66,15 @@ umap_data = umap_data %>%
   mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .))
 
 # Scale
-image_10x = umap_data %>%
-  dplyr::select(which(names(umap_data)=="rbc_sample_proportion"):which(names(umap_data)=="lipid_total_periphery_proportion"))
 image_100x_cells = umap_data %>%
   dplyr::select(!which(names(umap_data)=="rbc_sample_proportion"):which(names(umap_data)=="lipid_total_periphery_proportion")) %>%
   dplyr::select(contains("percentage")) %>%
   dplyr::select(-c(contains("_class"), contains("_class"), contains("quality"), contains("granularity"), contains("vacuolated"), contains("multinuclear")))
-image_100x_cellstatistics = umap_data %>%
-  dplyr::select(!which(names(umap_data)=="rbc_sample_proportion"):which(names(umap_data)=="lipid_total_periphery_proportion")) %>%
-  dplyr::select(-names(image_100x_cells))
 
-image_10x_scaled = sapply(image_10x, function(x) 2*(x - mean(x)) / sd(x))
-image_100x_cells_scaled = sapply(image_100x_cells, function(x) 10*(x - mean(x)) / sd(x))
-image_100x_cellstatistics_scaled = sapply(image_100x_cellstatistics, function(x) (x - mean(x)) / sd(x))
+image_100x_cells_scaled = sapply(image_100x_cells, function(x) 1*(x - mean(x)) / sd(x))
 ## Join
 image_100x_cells_scaled = as.data.frame(image_100x_cells_scaled)
-image_100x_cells_scaled$blasts_percentage = image_100x_cells_scaled$blasts_percentage*10; umap_data1 = image_100x_cells_scaled
-umap_data1 = cbind(cbind(image_10x_scaled, image_100x_cells_scaled), image_100x_cellstatistics_scaled)
+umap_data1 = image_100x_cells_scaled
 
 
 # UMAP
@@ -94,13 +86,22 @@ umap1 = umap1 %>%
   dplyr::rename(UMAP_1 = V1, UMAP_2 = V2) %>%
   cbind(umap_metadata)
 
+P
+umap1 = umap(umap_data1, scale = FALSE, n_neighbors = 15, seed = 10, metric = "cosine") %>% # metric = "correlation", "cosine"
+  as.data.frame()
+
+# Add ids
+umap1 = umap1 %>%
+  dplyr::rename(UMAP_1 = V1, UMAP_2 = V2) %>%
+  cbind(umap_metadata)
+
 # Plot
 ## BM Blast %
 umap1$b_leuk1 = ifelse(abs(umap1$bm_blast) > 20, 20, umap1$bm_blast)
-umap1$order = ifelse(is.na(umap1$b_leuk1), 0, umap1$b_leuk1)
+
 g = ggplot(data = umap1 %>%
              dplyr::arrange(order)) +
-  geom_point(aes(x = UMAP_1, y = UMAP_2, color = b_leuk1), size = 2, alpha=1) +
+  geom_point(aes(x = UMAP_1, y = UMAP_2, color = b_leuk1), size = 2, alpha=1) +#, size = 1) +
   scale_color_distiller(palette = "RdBu", name = "BM Blast (%)") + 
   theme_bw() +
   theme(
@@ -115,7 +116,7 @@ g = ggplot(data = umap1 %>%
     axis.title.x=element_blank(),
     axis.title.y=element_blank(),
     plot.background=element_blank()); g
-ggsave(plot = g, filename = "./UMAP_bmblast.png", bg = "white", width = 6, height = 6, units = "in", dpi = 300)
+ggsave(plot = g, filename = paste0(results, "_response/UMAP_bmblast.png"), bg = "white", width = 6, height = 6, units = "in", dpi = 300)
 
 ## WBC
 umap1$b_leuk1 = ifelse(abs(umap1$b_leuk) > 100, 100, umap1$b_leuk) 
@@ -126,17 +127,17 @@ g = ggplot(data = umap1 %>%
   scale_color_distiller(palette = "RdBu", name = "PB WBC (E9/L)") + 
   theme_bw() +
   theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        axis.line=element_blank(),
-        axis.text.x=element_blank(),
-        axis.text.y=element_blank(),
-        axis.ticks=element_blank(),
-        axis.title.x=element_blank(),
-        legend.position = "none",
-        axis.title.y=element_blank(),
-        plot.background=element_blank()); g
-ggsave(plot = g, filename = "./UMAP_wbc.png", bg = "white", width = 6, height = 6, units = "in", dpi = 300)
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.line=element_blank(),
+    axis.text.x=element_blank(),
+    axis.text.y=element_blank(),
+    axis.ticks=element_blank(),
+    axis.title.x=element_blank(),
+    legend.position = "none",
+    axis.title.y=element_blank(),
+    plot.background=element_blank()); g
+ggsave(plot = g, filename = paste0(results, "_response/UMAP_wbc.png"), bg = "white", width = 6, height = 6, units = "in", dpi = 300)
 
 ## WBC
 umap1 = umap1 %>%
@@ -167,5 +168,85 @@ g = ggplot(data = umap1 %>%
     axis.title.y=element_blank(),
     legend.position = "none",
     plot.background=element_blank()); g
-ggsave(plot = g, filename = "./UMAP_baseline_followup.png", bg = "white", width = 6, height = 6, units = "in", dpi = 300)
+ggsave(plot = g, filename = paste0(results, "_response/UMAP_baseline_followup.png"), bg = "white", width = 6, height = 6, units = "in", dpi = 300)
 
+
+##### DG SAMPLES #####
+
+
+# umap_data1 = umap_data1 %>%
+#   dplyr::mutate(rown = 1:nrow(.))
+# umap_data2 = umap_data1 %>%
+#   cbind(umap_metadata %>% dplyr::select(henkilotunnus, mgg_time_diff)) %>%
+#   dplyr::arrange(mgg_time_diff) %>%
+#   dplyr::filter(abs(mgg_time_diff) <= 60) %>%
+#   dplyr::group_by(henkilotunnus) %>%
+#   dplyr::slice(1) %>%
+#   ungroup()
+# umap_data1$dg_phase = ifelse(umap_data1$rown %in% umap_data2$rown, "DG", "FU")
+
+# UMAP
+umap_data2 = umap_data1 %>%
+  cbind(umap_metadata %>% dplyr::select(-matches(names(umap_data1)))) %>%
+  dplyr::mutate(rown = 1:nrow(.)) %>%
+  dplyr::arrange(mgg_time_diff) %>%
+  dplyr::filter(abs(mgg_time_diff) <= 60) %>%
+  dplyr::group_by(henkilotunnus) %>%
+  dplyr::slice(1) %>%
+  ungroup()
+umap_data0 = umap_data %>%
+  cbind(umap_metadata %>% dplyr::select(-matches(names(umap_data)))) %>%
+  dplyr::mutate(rown = 1:nrow(.)) %>%
+  dplyr::arrange(mgg_time_diff) %>%
+  dplyr::filter(abs(mgg_time_diff) <= 60) %>%
+  dplyr::group_by(henkilotunnus) %>%
+  dplyr::slice(1) %>%
+  ungroup()
+umap_data3 = umap_data2 %>%
+  dplyr::select(names(umap_data1))
+## Fill remaining NA with median
+umap_data3 = umap_data3 %>% 
+  mutate_all(~ifelse(is.na(.), median(., na.rm = TRUE), .))
+umap2 = umap(umap_data3, scale = FALSE, n_neighbors = 15, seed = 10, metric = "cosine") %>% # metric = "correlation", "cosine"
+  as.data.frame()
+
+# Add ids
+umap2 = umap2 %>%
+  dplyr::rename(UMAP_1 = V1, UMAP_2 = V2) %>%
+  cbind(umap_data0)
+
+
+# Plot
+umap2_long = umap2 %>%
+  dplyr::select(UMAP_1, UMAP_2, basophils_percentage, blasts_percentage, eosinophils_immature_percentage,
+                eosinophils_mature_percentage, eosinophils_percentage, erythroblasts_percentage,
+                macrophages_percentage, metamyelocytes_percentage, monocytes_percentage, myelocytes_percentage,
+                lymphocytes_percentage, neutrophils_percentage, plasma_cells_percentage,
+                proerythroblasts_percentage, promonocytes_percentage, promyelocytes_percentage) %>%
+  dplyr::mutate(eosinophils_immature_percentage = eosinophils_immature_percentage * eosinophils_percentage/100,
+                eosinophils_mature_percentage = eosinophils_mature_percentage * eosinophils_percentage/100) %>%
+  dplyr::select(-eosinophils_percentage) %>%
+  reshape2::melt(id.vars = c("UMAP_1", "UMAP_2"), variable.name = "Cells", value.name = "Proportion") %>%
+  dplyr::mutate(Cells = to_sentence_case(gsub("_", " ", gsub("_percentage", "", Cells)))) %>%
+  group_by(Cells) %>%
+  mutate(Proportion = log(Proportion+1)) %>%
+  ungroup() %>%
+  dplyr::arrange(Proportion)
+
+g = ggplot(data = umap2_long) +
+  geom_point(aes(x = UMAP_1, y = UMAP_2, color = Proportion), size = 2, alpha=1) +#, size = 1) +
+  scale_color_distiller(palette = "RdBu", direction = -1, name = "Proportion (Log %)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    axis.line=element_blank(),
+    axis.text.x=element_blank(),
+    axis.text.y=element_blank(),
+    axis.ticks=element_blank(),
+    axis.title.x=element_blank(),
+    legend.position = "bottom",
+    axis.title.y=element_blank(),
+    plot.background=element_blank()) +
+  facet_wrap("Cells", nrow = 2); g
+ggsave(plot = g, filename = paste0(results, "_response/UMAP_panel.png"), bg = "white", width = 15, height = 6, units = "in", dpi = 300)
